5c95ffe08fcb9dc59f2fc99da44f67191a1cb803
zshn1248 <zshn1248>
2025-10-26 12:18:49 +0500

    New Files Added

---
+++ Introduction_to_DataScience_With_Python/Different_Types_Of_Plots/Exercise_8.py
@@ -0,0 +1,9 @@
+# Create a histogram of the column weight from the DataFrame puppies
+plt.hist(puppies.weight)
+
+# Add labels
+plt.xlabel('Puppy Weight (lbs)')
+plt.ylabel('Number of Puppies')
+
+# Display
+plt.show()
